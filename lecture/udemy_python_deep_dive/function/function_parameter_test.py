a09f2454c6bd4b77d37a29394267ae105ae9886a
82244868 <gh.k@kt.com>
2024-02-20 09:51:39 +0900

    0220

--- lecture/udemy_python_deep_dive/function/function_parameter_test.py
+++ lecture/udemy_python_deep_dive/function/function_parameter_test.py
@@ -19,3 +19,50 @@ my_func(c=300, b=200, a=100)
 my_func(100, c=300, b=200)
 
 
+# [ Default Value 주의사항 1 ]
+# - 모듈이 Loaded 될때 모든 코드는 실행됨
+# - 함수의 경우 def ~ 정의 구문이 실행됨
+# - 따라서 argument 에 defalut Value 설정 시 주의해야함
+from datetime import datetime
+
+
+def my_log(msg, *, dt=datetime.now()):      # 잘못된 케이스
+    print('{0}: {1}'.format(dt, msg))
+
+# dt 자체는 모듈 로딩되는 시점에 이미 datetime.now()로 할당됨
+# 따라서 함수 호출 시점에, (언제 수행되던 이미 할당된 변수)를 그대로 '재사용'하게 됨
+
+
+my_log("1번")    # 2024-02-20 09:45:42.695373
+my_log("2번")    # 2024-02-20 09:45:42.695373    (동일)
+
+
+# 해결책
+def my_log2(msg, *, dt=None):
+    # if not dt:
+    #     dt = datetime.utcnow()
+    dt = dt or datetime.utcnow()
+    print('{0}: {1}'.format(dt, msg))
+
+
+my_log2("1번")    # 2024-02-20 00:47:55.015727
+my_log2("2번")    # 2024-02-20 00:47:55.015914
+
+
+# [ Default Value 주의사항 2 ]
+# - Argument 의 default Value 로 Mutable Object 사용시 주의
+# - 여러번 호출하게 되는데 서로 같은 Mutable object 참조/활용하게 되어버림
+def add_item(item, *, my_list=[]):
+    my_list.append(item)
+    return my_list
+
+
+my_list = add_item('A')
+print(my_list)      # ['A']
+
+# 다른 호출
+my_list2 = add_item('가')
+print(my_list2)     # ['A', '가']
+
+# 즉, my_list == mylist_2
+print(my_list is my_list2)
